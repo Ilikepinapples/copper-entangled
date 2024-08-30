87e04ce5dcc1a872ea9f635a49ce64017caf4b1e
Ilikepinapples <private-email@local.dev>
2024-08-30 01:16:28 +0100

    Initial commit

---
+++ src/client/java/entity0/copperentangled/CopperEntangledClient.java
@@ -0,0 +1,10 @@
+package entity0.copperentangled;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CopperEntangledClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
